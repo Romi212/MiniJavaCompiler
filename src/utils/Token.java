224baff79000c693ea16c34a3560bf27e86b7413
Romi212 <104693764+Romi212@users.noreply.github.com>
2024-08-24 12:01:15 -0300

    initial structure of the first part of the proyect

---
+++ src/utils/Token.java
@@ -0,0 +1,40 @@
+package utils;
+
+public class Token {
+
+    private String token;
+    private String lexeme;
+    private int line;
+
+    private int column;
+
+    public Token(String token, String lexeme, int line) {
+        this.token = token;
+        this.lexeme = lexeme;
+        this.line = line;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getLexeme() {
+        return lexeme;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+
+    public String toString() {
+        return "(" + token + ", " + lexeme + ", " + line + ")";
+    }
+}
